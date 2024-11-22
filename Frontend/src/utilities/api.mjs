f7386a60d47032ae1827d518b736c5f9137ca654
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-11-22 08:56:48 -0500

    I believe I have the frontend api connected. so far no errors for the api.mjs

---
+++ Frontend/src/utilities/api.mjs
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const GIPHY_API_KEY = import.meta.env.VITE_GIPHY_API_KEY; // API key gets stored in .env
+const GIPHY_ENDPOINT = 'https://api.giphy.com/v1/gifs/halloween&limit=25';
+
+const fetchRandomGif = async (tag = 'Halloween') => {
+    try {
+        const response = await axios.get(GIPHY_ENDPOINT, {
+            params: {
+                api_key: GIPHY_API_KEY,
+            },
+        });
+        return response.data.data.images.original.url; // Return GIF URL
+    } catch (error) {
+        console.error('Error fetching GIF:', error);
+    return null;
+    }
+};
+
+export { fetchRandomGif };
